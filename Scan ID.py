eb0692259f9be1ec8ded10ffb9ac74069298e5a2
brn5198 <brn5198@gmail.com>
2016-05-02 11:27:24 -0400

    Scan ID & Valid ID check complete.

---
+++ Scan ID.py
@@ -0,0 +1,17 @@
+__author__ = 'student'
+import time
+import pickle
+
+while True:
+    s_id = input('Choose what you would like to do from the following options: \n1.)Scan student id?,\n2.)View Report, \n3.)Quit? \nType the option number here: ')
+    if s_id in ['1']:
+        try:
+            s_id = input('Scan your ID here: ')
+            print('%s signed in at - %s' % (student_info[s_id], time.strftime("%I:%M %p")))
+        except KeyError:
+            print('This is an invalid ID. Please try again.')
+
+    elif s_id in ['2']:
+        pass# the view report group write your code here:
+    else:
+        pass# the quit group write your code here:
