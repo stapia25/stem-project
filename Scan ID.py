1fce34043aaae7ade06f6062a82deecdbff65594
rosysweet11 <riyaj498@gmail.com>
2016-05-04 11:28:51 -0400

    Update Scan Id
    By Riya Yahira Cristina it tries to mark people late by the type in l L thing

--- Scan ID.py
+++ Scan ID.py
@@ -4,13 +4,14 @@ import pickle
 with open("student_info.p","rb") as f:
          student_info = pickle.load(f)
 while True:
-    s_id = input('Choose what you would like to do from the following options: \n1.)Scan student id?,\n2.)View Report, \n3.)Quit? \nType the option number here: ')
-    if s_id in ['1']:
+     if s_id in ['1']:
         try:
-            s_id = input('Scan your ID here: ')
-            print('%s signed in at - %s' % (student_info[s_id], time.strftime("%I:%M %p")))
+                scan = input('Scan your ID here: ')
+                print('%s signed in at - %s' % (student_info[scan], time.strftime("%I:%M %p")))
         except KeyError:
-            print('This is an invalid ID. Please try again.')
+                print('This is an invalid ID. Please try again.')
+        if scan in ['l', 'L']:
+                 print('%s checked in late - %s' % (student_info[s_id], time.strftime("%I:%M %p")))
 
     elif s_id in ['2']:
         pass# the view report group write your code here:
